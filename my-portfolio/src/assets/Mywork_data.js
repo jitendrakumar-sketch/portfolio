628d582ecc04dbff86cf4786edd07c6e9f591e5d
Jitendra Kumar <jk398206@gmail.com>
2026-01-13 21:37:04 +0530

    Update Mywork_data.js

--- my-portfolio/src/assets/Mywork_data.js
+++ my-portfolio/src/assets/Mywork_data.js
@@ -1,5 +1,5 @@
-import project1_img from './Project-1-2.png'
-import project2_img from './Project-2-1.png'
+import project1_img from './Project_1_2.png'
+import project2_img from './Project_2_1.png'
 
 
 const Mywork_data = [
