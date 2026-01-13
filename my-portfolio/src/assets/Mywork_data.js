e0f1630906cf2740043c886690dede85ae6d7742
Jitendra Kumar <jk398206@gmail.com>
2026-01-13 21:26:22 +0530

    Update Mywork_data.js

--- my-portfolio/src/assets/Mywork_data.js
+++ my-portfolio/src/assets/Mywork_data.js
@@ -1,9 +1,6 @@
-import project1_img from '../assets/project-1-2.png'
-import project2_img from '../assets/project-2-1.png'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project1_img from '../assets/Project-1-2.png'
+import project2_img from '../assets/Project-2-1.png'
+
 
 const mywork_data = [
     {
